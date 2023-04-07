cfc37602bea54e2238bbfba44b5cce0bdcf34e50
Melk de Sousa <melk.sousap@gmail.com>
2023-04-07 09:59:12 -0300

    add zod and rhf to handle form signin

--- src/components/ButtonFilled.tsx
+++ src/components/ButtonFilled.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { TouchableOpacityProps } from 'react-native'
 import { TouchableOpacity } from '../styles'
+import { Loading } from './Loading'
 
-export type ButtonFilledProps = TouchableOpacityProps
+export type ButtonFilledProps = TouchableOpacityProps & {
+  loading?: boolean
+}
 
-export const ButtonFilled = (props: ButtonFilledProps) => {
+export const ButtonFilled = ({
+  children,
+  loading = false,
+  onPress,
+  ...props
+}: ButtonFilledProps) => {
   return (
     <TouchableOpacity
       className="w-full bg-product-700 items-center p-4 rounded-md"
+      disabled={loading}
+      onPress={loading ? undefined : onPress}
+      style={{
+        opacity: loading ? 0.5 : 1,
+      }}
       {...props}
-    />
+    >
+      {loading && <Loading />}
+      {!loading && children}
+    </TouchableOpacity>
   )
 }
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,41 @@
 import React from 'react'
 
-import { Envelope, Key } from 'phosphor-react-native'
+import { Key, User } from 'phosphor-react-native'
 import Logo from '../assets/svg/logo.svg'
 import { Input } from '../components/Input'
 
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { ButtonFilled } from '../components/ButtonFilled'
 import { Space } from '../components/Space'
 import { KeyboardAvoidingView, Text, View, colors, sizes } from '../styles'
 
+import { zodResolver } from '@hookform/resolvers/zod'
+
+export const signInSchema = z.object({
+  username: z.string().min(5, {
+    message: 'O nome de usuário precisa ter no mínimo 5 caracteres',
+  }),
+  password: z
+    .string()
+    .min(8, { message: 'A senha precisa ter no mínimo 8 caracteres' }),
+})
+
+export type SignInFormData = z.infer<typeof signInSchema>
+
 export const SignIn = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInFormData>({
+    resolver: zodResolver(signInSchema),
+  })
+
+  const onSubmit = (data: SignInFormData) => {
+    console.log(data)
+  }
+
   return (
     <KeyboardAvoidingView
       overScrollMode="never"
@@ -21,29 +48,46 @@ export const SignIn = () => {
           Acesse sua conta
         </Text>
 
-        <Input
-          placeholder="E-mail"
-          textContentType="emailAddress"
-          keyboardType="email-address"
-          enablesReturnKeyAutomatically
-          returnKeyType="next"
-          leftElement={<Envelope color={colors.gray[300]} size={sizes[7]} />}
+        <Controller
+          control={control}
+          name="username"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              placeholder="Nome de usuário"
+              textContentType="nickname"
+              enablesReturnKeyAutomatically
+              returnKeyType="next"
+              leftElement={<User color={colors.gray[300]} size={sizes[7]} />}
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
 
         <Space className="h-4" />
 
-        <Input
-          textContentType="password"
-          enablesReturnKeyAutomatically
-          placeholder="Senha"
-          returnKeyType="done"
-          leftElement={<Key color={colors.gray[300]} size={sizes[7]} />}
-          secureTextEntry
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <Input
+              textContentType="password"
+              enablesReturnKeyAutomatically
+              placeholder="Senha"
+              returnKeyType="done"
+              leftElement={<Key color={colors.gray[300]} size={sizes[7]} />}
+              secureTextEntry
+              onBlur={onBlur}
+              onChangeText={onChange}
+              value={value}
+            />
+          )}
         />
 
         <Space className="h-4" />
 
-        <ButtonFilled>
+        <ButtonFilled loading={isSubmitting} onPress={handleSubmit(onSubmit)}>
           <Text className="font-roboto font-bold text-lg text-white">
             Entrar
           </Text>
